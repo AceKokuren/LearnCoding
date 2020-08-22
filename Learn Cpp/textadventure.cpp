98b915fb717b2117038abade540812cac989fd9f
AceKokuren <66668434+AceKokuren@users.noreply.github.com>
2020-08-22 16:58:27 +0100

    Began right branch, need to creat rng for panther attack!

--- Learn Cpp/textadventure.cpp
+++ Learn Cpp/textadventure.cpp
@@ -2,7 +2,7 @@
 #include <conio.h>
 
 int main() {
-
+//The intro to the adventure, a talk with the king and then choice to accept quest!
   std::cout << "     -----------------Welcome Adventurer!-----------------\n\n";
   std::cout << "You have been summoned by the King for a very important mission!\n";
   std::cout << "As you enter the King's Chamber, you hear: 'Ah Welcome Adventurer!\nTell me, who do I have the plasure of meeting today?'\n";
@@ -18,12 +18,12 @@ int main() {
 
   int answer = 0;
   std::cin >> answer;
-
+//A loop to compare input to valid option, will only progress when a valid input has been given
   while (answer != 1 && answer != 2 && answer != 3) {
     std::cout << "Please enter 1, 2 or 3 only!\n";
     std::cin >> answer;
   }
-
+//Case statement for each option, 1 and 3 will progress the story, 2 will end programme
   switch (answer) {
     case 1:
     std::cout << "'Excellent! Here is the message, make haste " << name << "!'\n";
@@ -38,6 +38,7 @@ int main() {
     std::cout << "Y/N";
     std::string yn;
     std::cin >> yn;
+    //Another loop to wait for a valid input
     while (yn !="Y" && yn != "N") {
       std::cout << "Please enter Y or N only!\n";
       std::cin >> yn;      
@@ -51,7 +52,7 @@ int main() {
 	return 0;
     }
   }
-  
+  //First choice, a fork in the road
   std::cout << "\nYou make your way out of town and come to a fork in the road!\n";
   std::cout << "One leads down the main path but is likely to make you late for your deadline!\n";
   std::cout << "The other is a shortcut that will get you to Bangrey on time, but many dangers lurk down this path!\n";
@@ -72,8 +73,9 @@ int main() {
   else if (fork == "Right" || fork == "right") {
     std::cout << "You go right, down the forest path!\n";
   }*/
-
+//Allows user to input invalid option 5 times
   for (int i  = 0; i < 5; i++) {
+    //Left branch of the fork
     if (fork == "Left" || fork == "left") {
     std::cout << "You start heading down the main road!\n";
     std::cout << "You come across an overturned cart!\n";
@@ -81,8 +83,15 @@ int main() {
     std::cout << "Do you Help or Move passed?\n";
     break;
   }
+  //Right branch of the fork
    else if (fork == "Right" || fork == "right") {
     std::cout << "You go right, down the forest path!\n";
+    getch();
+    std::cout << "As you make your way through the woods, it becomes harder to find safe ground\n";
+    std::cout << "to navgate your mount safely!\n";
+    getch();
+    std::cout << "After a moment, your horse stops, and you hear the trees rustle!" << std::endl;
+    std::cout << "Suddenly, a panther launches itself at you!" << std::endl;
     break;
   }
   else {
