3c5cc9f8b624a74f5cce44a2faebf0a36277c96c
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2020-10-22 15:29:53 +0100

    Created void function with instructions

--- Learn Cpp/UFO Game/ufo_functions.cpp
+++ Learn Cpp/UFO Game/ufo_functions.cpp
@@ -3,8 +3,10 @@
 
 // Define functions
 void greet() {
-  std::cout << "=====================\n";
-  std::cout << "    UFO: The Game\n";
-  std::cout << "=====================\n";
-  
+   std::cout << "=====================\n";
+   std::cout << "    UFO: The Game\n";
+   std::cout << "=====================\n";
+   std::cout << "Instructions: Save your friend from alien\n";
+   std::cout << "abduction by guessing the letters\n";
+   std::cout << "in the codeword!" << std::endl;  
 }
