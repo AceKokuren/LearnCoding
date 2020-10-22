a582b23fb685f2a33a773b5f9ae689abfc6701c8
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2020-10-22 16:29:05 +0100

    Added misses function and a for loop to test misses function

--- Learn Cpp/UFO Game/ufo.cpp
+++ Learn Cpp/UFO Game/ufo.cpp
@@ -6,5 +6,12 @@ int main() {
     greet();
     std::string codeword = "codecademy", answer = "_ _ _ _ _ _ _ _ _ _";
     int misses = 0;
-    
+    for (int i = 0; i < 8; i++) {
+        display_misses(misses);
+         misses++;
+    if (misses == 7) {
+        std::cout << "Game Over!\n";
+        return 0;
+   }
+  }
 }
--- Learn Cpp/UFO Game/ufo_functions.hpp
+++ Learn Cpp/UFO Game/ufo_functions.hpp
@@ -1,1 +1,2 @@
 void greet();
+void display_misses(int misses);
