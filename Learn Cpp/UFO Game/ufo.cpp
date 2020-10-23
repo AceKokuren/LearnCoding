01dca34d7c2afcb8a22b9dab23fa299291b82024
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2020-10-23 11:33:58 +0100

    Replaced for loop with while loop, created victory scenario.

--- Learn Cpp/UFO Game/ufo.cpp
+++ Learn Cpp/UFO Game/ufo.cpp
@@ -6,12 +6,16 @@ int main() {
     greet();
     std::string codeword = "codecademy", answer = "_ _ _ _ _ _ _ _ _ _";
     int misses = 0;
-    for (int i = 0; i < 8; i++) {
+    while (misses < 7 && answer != codeword) {
         display_misses(misses);
-         misses++;
+        misses++;
+    }
     if (misses == 7) {
         std::cout << "Game Over!\n";
         return 0;
    }
-  }
+   else if (answer == codeword) {
+       std::cout << "Congratulations, you saved the day!\nVictory!" << std::endl; 
+   }
+   
 }
