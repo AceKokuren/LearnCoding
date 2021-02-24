11a3c9b2069c1d577505ab39f9667f402b690162
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2021-02-24 10:58:32 +0000

    Created programme that displays my initials

---
+++ Learn Cpp/initials.cpp
@@ -0,0 +1,13 @@
+//I have had a motorbike crash and walked away without a scratch
+
+#include <iostream>
+
+int main(){
+  std::cout << " C C C " << "     A   " << "  B B B B \n";
+  std::cout << "C     C " << "  A   A " << "  B      B\n";
+  std::cout << "C       " << " A     A " << " B      B\n";
+  std::cout << "C       " << " A A A A  " << "B B B B\n";
+  std::cout << "C       " << " A     A " << " B      B\n";
+  std::cout << "C     C " << " A     A  " << "B      B\n";
+  std::cout << " C C C  " << " A     A  " << "B B B B\n";
+}
