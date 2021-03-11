a249d32e7264292a68ee1dac43dea571aae0d19c
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2021-03-11 16:33:28 +0000

    Added ability for user input

--- Learn Cpp/palindrome.cpp
+++ Learn Cpp/palindrome.cpp
@@ -6,25 +6,30 @@ bool is_palindrome(std::string text) {
 
 std::vector <char> reversed_text;
 
-  int total = 0;
   for (int i = text.size() - 1; i >= 0; i--) {
     reversed_text.push_back(text[i]);
   }
-  for (int j = 0; j < text.size() - 1; j++) {
-    if (text[j] == reversed_text[j]) {
-      total++;
+  for (int j = 0; j < text.size(); j++) {
+    if (text[j] != reversed_text[j]) {
+      std::cout << text << " is not a palindrome!\n";
+      return false;
     }
   }
-  if (total == reversed_text.size() - 1) {
-    return true;
-  }
-  else {
-    return false;
-  }
+  std::cout << text << " is a palindrome!\n";
+  return true;
 }
 int main() {
   
-  std::cout << is_palindrome("madams") << "\n";
+  bool another = true;
+  std::string input;
+
+  while (another) {
+    std::cout << "Enter word to check for palindrome: ";
+    std::cin >> input;
+    is_palindrome(input);
+  }
+
+  /*std::cout << is_palindrome("madam") << "\n";
   std::cout << is_palindrome("ada") << "\n";
-  std::cout << is_palindrome("lovelacel") << "\n";
+  std::cout << is_palindrome("lovelace") << "\n";*/
 }
