d7b93ab132e69c1b627af0acfb73b2e1e9928c19
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2021-03-11 15:37:49 +0000

    Corrected palindrome to iterate through whole of text, not returning after first letter

--- Learn Cpp/palindrome.cpp
+++ Learn Cpp/palindrome.cpp
@@ -6,24 +6,25 @@ bool is_palindrome(std::string text) {
 
 std::vector <char> reversed_text;
 
+  int total = 0;
   for (int i = text.size() - 1; i >= 0; i--) {
     reversed_text.push_back(text[i]);
-    
-    for (int j = 0; j < reversed_text.size(); j++) {
-      if (text[j] == reversed_text[j]) {
-        return true;
-      }
-      else {
-        return false;
-      }
-    } 
   }
-  return 0;
+  for (int j = 0; j < text.size() - 1; j++) {
+    if (text[j] == reversed_text[j]) {
+      total++;
+    }
+  }
+  if (total == reversed_text.size() - 1) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
-
 int main() {
   
-  std::cout << is_palindrome("madam") << "\n";
+  std::cout << is_palindrome("madams") << "\n";
   std::cout << is_palindrome("ada") << "\n";
-  std::cout << is_palindrome("lovelace") << "\n";
+  std::cout << is_palindrome("lovelacel") << "\n";
 }
