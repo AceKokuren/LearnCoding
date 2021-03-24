cf38cb7bf0d162329a47ca165482279cd546da44
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2021-03-24 09:25:29 +0000

    Created skeleton for app

---
+++ Learn Cpp/Class Test/Object of your Affection/app.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "profile.hpp"
+
+int main() {
+
+  
+
+}
---
+++ Learn Cpp/Class Test/Object of your Affection/profile.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include "profile.hpP"
+
+class Profile {
+  
+  public:
+};
