90f19265a9cdf1b58ed051a09610fa52ebafcf8a
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2021-03-24 09:40:17 +0000

    Added Constructor for a profile

--- Learn Cpp/Class Test/Object of your Affection/profile.cpp
+++ Learn Cpp/Class Test/Object of your Affection/profile.cpp
@@ -1,2 +1,7 @@
 #include <iostream>
 #include "profile.hpp"
+
+Profile::Profile(int new_age, std::string new_name, std::string new_city, std::string new_country, std::string new_pronoun = "They/Them")
+    :   age(new_age), name(new_name), city(new_city), country(new_country), pronoun(new_pronoun) {
+        
+    }
