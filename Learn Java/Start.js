eb5a1709c840945404f5b52dffa0379a02c74f9d
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2021-03-30 13:12:37 +0100

    Created 2 new starting files with basics of JS

---
+++ Learn Java/Start.js
@@ -0,0 +1,3 @@
+let str1 = "Hello World\nIt's a beautiful day!";
+
+console.log(Math.ceil(Math.random() * 20));
---
+++ Learn Java/numericalOperators.js
@@ -0,0 +1,22 @@
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+levelUp += 5;          //Increases levelUp by 5
+powerLevel -=100;      //Subtracts 100 from powerLevel 
+multiplyMe *= 11;      //Multiplies multiplyMe by 11 
+quarterMe /= 4;        //Divides quarterMe by 4
+
+for (let i = 0; i < 20; i++) {
+  levelUp++;
+}
+  console.log(levelUp);
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
