be2ae1a207fd067a23b766a319118743dda98fbd
AceKokuren <66668434+AceKokuren@users.noreply.github.com>
2021-03-30 18:55:19 +0100

    Made some adjustments and testing concatination

--- Learn Java/concatination.js
+++ Learn Java/concatination.js
@@ -5,3 +5,8 @@ str2 = ' my name is ';
 str3 = "NAME";
 str4 = str1 += str2 += str3;
 console.log(str4);
+
+let convertHours;
+const hours = 8;
+const mins = hours * 60; 
+console.log(`You have ${mins} minutes remaining!`);
