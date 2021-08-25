e22e9a1a98d285013f6aa17f9f8a531eddf60e52
AceKokuren <66668434+AceKokuren@users.noreply.github.com>
2021-08-25 20:50:20 +0100

    Cleaned up code and fixed .push method on dish

--- Learn Java/Objects/mealMaker.js
+++ Learn Java/Objects/mealMaker.js
@@ -1,46 +1,39 @@
 const menu = {
-    _courses: {
-      appetizers: [],
-      mains: [],
-      desserts: []
-    },
-    set appetizers(appetizerIn) {
-      this._courses.appetizers = appetizerIn;
-    },
-    get appetizers() {
-        return this.appetizers;
-    },
-    set mains(mainsIn) {
-  
-    },
-    get mains() {
-  
-    },
-    set desserts(dessertsIn) {
-  
-    },
-    get desserts() {
-  
-    },
-    get courses() {
-      return {
-        appetizers: this.appetizers,
-        mains: this.mains,
-        desserts: this.desserts
-      };
-    },
-    addDishToCourse(courseName, dishName, dishPrice) {
-      let dish = {
-        name: dishName,
-        price: dishPrice
-      };
-      switch (courseName) {
-        case 'appetizers':
-          this._courses.appetizers.push(dish)
-      }
-    }
-  };
-  
-  menu.appetizers = 'Calamari';
-  console.log(menu._courses.appetizers);
-  menu.addDishToCourse('appetizers', 'Soup', 3.49);
+  _courses: {
+    appetizers: [],
+    mains: [],
+    desserts: []
+  },
+  set appetizers(appetizerIn) {
+    this._courses.appetizers = appetizerIn;
+  },
+  get appetizers() {
+      return this.appetizers;
+  },
+  set mains(mainsIn) {
+
+  },
+  get mains() {
+
+  },
+  set desserts(dessertsIn) {
+
+  },
+  get desserts() {
+
+  },
+  get courses() {
+    return {
+      appetizers: this.appetizers,
+      mains: this.mains,
+      desserts: this.desserts
+    };
+  },
+  addDishToCourse(courseName, dishName, dishPrice) {
+    let dish = {
+      name: dishName,
+      price: dishPrice
+    };
+    return this._courses[courseName].push(dish);
+  },
+};
