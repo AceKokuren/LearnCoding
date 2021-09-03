9fc512381faf8607d26d7a48f890f1d72d44a0d2
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2021-09-03 15:18:10 +0100

    Added meal maker project

---
+++ Learn Java/Objects/mealMaker.js
@@ -0,0 +1,41 @@
+const menu = {
+    _courses: {
+      appetizers: [],
+      mains: [],
+      desserts: []
+    },
+    set appetizers(appetizerIn) {
+      this._courses.appetizers = appetizerIn;
+    },
+    get appetizers() {
+        return this.appetizers;
+    },
+    set mains(mainsIn) {
+  
+    },
+    get mains() {
+  
+    },
+    set desserts(dessertsIn) {
+  
+    },
+    get desserts() {
+  
+    },
+    get courses() {
+      return {
+        appetizers: this.appetizers,
+        mains: this.mains,
+        desserts: this.desserts
+      };
+    },
+    addDishToCourse(courseName, dishName, dishPrice) {
+      let dish = {
+        name: dishName,
+        price: dishPrice
+      };
+      return this._courses[courseName].push(dish);
+    }
+  };
+  
+menu.addDishToCourse('main', 'Fish', 7.69);
