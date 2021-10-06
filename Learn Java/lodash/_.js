dfb99f5f548aeb0cb41f47902a73f7fa059a3251
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2021-10-06 15:46:55 +0100

    added .clamp method

--- Learn Java/lodash/_.js
+++ Learn Java/lodash/_.js
@@ -1,4 +1,13 @@
-let _ = {};
+const _ = {
 
-// Do not write or modify code below this line.
-module.exports = _;
+    clamp (currentValue, lowerBound, upperBound) {
+  
+      const low = Math.max(currentValue, lowerBound);
+      const clamped = Math.min(low, upperBound);
+      return clamped
+    }
+  };
+  console.log(_.clamp(5, 3, 4))
+
+  // Do not write or modify code below this line.
+  module.exports = _;
