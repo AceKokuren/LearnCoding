3db1eb9dc6ff4c680dee62f6e9bf08dbdb670654
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2021-11-02 08:33:55 +0000

    Added .inRange and .words to lodash, started .pad

--- Learn Java/lodash/_.js
+++ Learn Java/lodash/_.js
@@ -1,13 +1,40 @@
 const _ = {
-
-    clamp (currentValue, lowerBound, upperBound) {
-  
-      const low = Math.max(currentValue, lowerBound);
-      const clamped = Math.min(low, upperBound);
-      return clamped
+  clamp (currentValue, lowerBound, upperBound) {
+    const low = Math.max(currentValue, lowerBound);
+    const clamped = Math.min(low, upperBound);
+    return clamped
+    },
+  inRange (number, startValue, endValue) {
+    if (!endValue) {
+      endValue = startValue;
+      startValue = 0;
     }
-  };
-  console.log(_.clamp(5, 3, 4))
-
+    if (startValue > endValue) {
+      let swap = endValue;
+      endValue = startValue;
+      startValue = swap;
+    }
+    if (number < startValue || number >= endValue) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  },
+  words (string) {
+    return string.split(' ');
+  },
+  pad (string, padding) {
+    if (string.length === padding) {
+      return string;
+    }
+    const padStart = Math.floor((padding - string.length) / 2);
+    //console.log(padStart);
+  }
+};
+  //console.log(_.clamp(5, 3, 4));
+  //console.log(_.inRange(5, 1, 10));
+  //console.log(_.words('Hello, my name is Cameron'));
+  console.log(_.pad('Hello', 6));
   // Do not write or modify code below this line.
   module.exports = _;
