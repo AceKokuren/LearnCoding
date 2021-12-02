a5f028d6570c1053add30167be7b1fbd8ed0edf3
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2021-12-02 12:34:51 +0000

    Added functionality to quote() function

--- Projects/Mixed Messages/main.js
+++ Projects/Mixed Messages/main.js
@@ -1,6 +1,6 @@
 const characters = ['Captain Jack Sparrow'];
 const line1 = ['This is the day you will always remember as the day'];
-const line2 = ['you almost caught Captain Jack Sparrow'];
+const line2 = ['you almost caught, Captain Jack Sparrow'];
 
 const randomElement = (arr) => {
     let el  =  Math.floor(Math.random() * arr.length);
@@ -8,5 +8,10 @@ const randomElement = (arr) => {
 }
 
 const quote = () => {
-    let char = randomElement(characters)
+    let char = randomElement(characters);
+    let part1 = randomElement(line1);
+    let part2 = randomElement(line2);
+
+    console.log(`${part1},\n ${part2} \n          -${char}`)
 }
+quote();
