70f83b14d53420a53faf5fcc92013010caedd33c
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2021-12-02 11:56:07 +0000

    Fixed randomElement return to return the element at n index

--- Projects/Mixed Messages/main.js
+++ Projects/Mixed Messages/main.js
@@ -3,9 +3,10 @@ const line1 = ['This is the day you will always remember as the day'];
 const line2 = ['you almost caught Captain Jack Sparrow'];
 
 const randomElement = (arr) => {
-    return Math.floor(Math.random() * arr.length);
+    let el  =  Math.floor(Math.random() * arr.length);
+    return arr[el];
 }
 
 const quote = () => {
-    
+    let char = randomElement(characters)
 }
