379969936c2020345816033b258e2f73f2ea9f74
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2021-12-02 10:30:06 +0000

    Added functionality to randomElement()

--- Projects/Mixed Messages/main.js
+++ Projects/Mixed Messages/main.js
@@ -2,6 +2,6 @@ const characters = ['Captain Jack Sparrow'];
 const line1 = ['This is the day you will always remember as the day'];
 const line2 = ['you almost caught Captain Jack Sparrow'];
 
-const randomElement = () => {
-    
+const randomElement = (arr) => {
+    return Math.floor(Math.random() * arr.length);
 }
