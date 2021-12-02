4d22a1902178c5d0f10a6e20fafc35292e8ceb67
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2021-12-02 09:00:10 +0000

    Added 1 element to each array

--- Projects/Mixed Messages/main.js
+++ Projects/Mixed Messages/main.js
@@ -1,3 +1,4 @@
-const characters = [];
-const line1 = [];
-const line2 = [];
+const characters = ['Captain Jack Sparrow'];
+const line1 = ['This is the day you will always remember as the day'];
+const line2 = ['you almost caught Captain Jack Sparrow'];
+
