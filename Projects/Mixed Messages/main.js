18ae960c4c8265d0a25506a684eef6baebde7632
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2021-12-02 10:02:52 +0000

    Created empty random number generator function

--- Projects/Mixed Messages/main.js
+++ Projects/Mixed Messages/main.js
@@ -1,3 +1,7 @@
 const characters = ['Captain Jack Sparrow'];
 const line1 = ['This is the day you will always remember as the day'];
 const line2 = ['you almost caught Captain Jack Sparrow'];
+
+const randomElement = () => {
+    
+}
