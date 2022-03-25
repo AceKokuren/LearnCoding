1bfe709ba2db63e9457194caa557f93dee21a571
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2022-03-25 09:23:53 +0000

    Refactored lifePhase to be more concise using an object and nullish coalescing

--- Learn Java/lifePhase.js
+++ Learn Java/lifePhase.js
@@ -1,32 +1,12 @@
 // Write your function here:
+const ages = {3: 'baby', 12: 'child', 19: 'teen', 64: 'adult', 140: 'senior citizen'};
+
 const lifePhase = age => {
-    if (age >= 0 && age <= 3) {
-      return 'baby';
-    }
-    else if (age > 3 && age <= 12) {
-      return 'child';
-    }
-    else if (age > 12 && age <= 19) {
-      return 'teen';
-    }
-    else if (age > 19 && age <= 64) {
-      return 'adult';
-    }
-    else if (age > 64 && age <= 140) {
-      return 'senior citizen';
-    }
-    else {
-      return 'This is not a valid age';
-    }
-  }
+  const ageRange = Object.keys(ages).find(a => age >= 0 && age <= a) ?? 0
+  return ages[ageRange] ?? 'This is not a valid age'
+}
   
-  console.log(lifePhase(-1));
-  
-  
-  
-  
-  
-  // Uncomment the line below when you're ready to try out your function
-  // console.log(lifePhase(5)) //should print 'child'
-  
-  // We encourage you to add more function calls of your own to test your code!
+  console.log(lifePhase(1)); //Prints 'baby'
+  console.log(lifePhase(4)); //Prints 'child'
+  console.log(lifePhase(-5));
+  console.log(lifePhase(0));
