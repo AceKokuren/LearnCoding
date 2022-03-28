8a0da5436bee651f13814a810243f345b79145b0
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2022-03-28 13:52:38 +0100

    Removed comment and whitespace, changed compareGuesses to function declaration

--- Learn Java/number-guesser-starting/script.js
+++ Learn Java/number-guesser-starting/script.js
@@ -2,11 +2,9 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
 const generateTarget = () => Math.floor(Math.random() * 10);
 const absoluteDistance = (guess, target) => Math.abs(guess - target);
-
-const compareGuesses = (userGuess, computerGuess, targetNumber) => {
+function compareGuesses(userGuess, computerGuess, targetNumber) {
     let userDiff = absoluteDistance(userGuess, targetNumber);
     let computerDiff = absoluteDistance(computerGuess, targetNumber);
     if (userGuess < 0 || userGuess > 10) {
