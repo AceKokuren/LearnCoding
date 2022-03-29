2667fd90935093d22db271b588fa51afaa314d6a
Mr C Black <66668434+AceKokuren@users.noreply.github.com>
2022-03-29 16:37:16 +0100

    Refactored functions to arrow function

--- Learn Java/digitCombos.js
+++ Learn Java/digitCombos.js
@@ -1,16 +1,18 @@
-const createCombo = (numOfDigits) => String(Math.floor(Math.random() * Math.pow(10, numOfDigits))).padStart(numOfDigits, '0'); //4 Digit Number Generator with leading 0s
-
 //Adds all possible combinations of set number of digit numbers to our combo array
 function digitCombos(numOfDigits) {
     let combos = [];
-    do {
-        let num = createCombo(numOfDigits);
-        if (!combos.includes(num)) {
-            combos.push(num);
-        }
-    }   while (combos.length < Math.pow(10, numOfDigits));
+    let i = 0;
+    while (i !== Math.pow(10, numOfDigits)) {
+        combos.push(String(i).padStart(numOfDigits, '0'));
+        i++;
+    }
     return combos;
 }
 
-console.log(digitCombos(4)); //Prints Array of all possible 4 digit numbers in random order
-console.log(digitCombos(2).sort()); //Prints Array of all possible 2 digit numbers in order
+const numCombos = digits => new Array(Math.pow(10, digits)).fill().map((a,b) => b.toString().padStart(digits, '0'));
+console.log(new Array(10).fill().map((a,b) => b.toString()));
+
+//console.log(numCombos(4));
+//console.log(numCombos(5));
+//console.log(digitCombos(4)); //Prints Array of all possible 4 digit numbers in order
+//console.log(digitCombos(5)); //Prints Array of all possible 5 digit numbers in order
