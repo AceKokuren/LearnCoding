cf650f6725e9f92312c9b1c28b792429c5a69d73
AceKokuren <cammy.black998@gmail.com>
2022-06-21 13:39:00 +0100

    Added a scoreboard to website and added some style to the page

--- Learn Javascript/rpsv3/script.js
+++ Learn Javascript/rpsv3/script.js
@@ -1,6 +1,12 @@
 let output = document.getElementById('output');
 let startGame = document.getElementById("start");
 
+let humanScore = document.getElementById("human-score");
+let compScore = document.getElementById("computer-score");
+
+let scoreH = 0;
+let scoreC = 0;
+
 //Function to generate a random number between 1 and 3 then choose rock, paper or scissors.
 const compGuess = () => {
   const num = Math.ceil(Math.random() * 3);
@@ -16,31 +22,38 @@ const compGuess = () => {
   }
 }
 
+const checkHumanIsValid = human => human !== "rock" && human !== "paper" &&  human !== "scissors" ? false : true;
+
 //Function to compare guesses, throwing an alert if an invalid entry is given
 const compareGuesses = (human, computer) => {
-  if (human !== 'rock' && human !== 'paper' && human !== 'scissors') {
-    alert("Invalid Option, try again");
-    return 0;
-  }
-  else if (human === computer) {
+  if (human === computer) {
     return `You chose ${human}, computer chose ${computer}, it's a draw!`;
   }
   else if (human === 'rock' && computer === 'scissors' || human === 'paper' && computer === 'rock' || human === 'scissors' && computer === 'paper') {
+    scoreH++;
     return `You chose ${human}, Computer chose ${computer}! You win!`;
   }
   else if (human === 'rock' && computer === 'paper' || human === 'paper' && computer === 'scissors' || human === 'scissors' && computer === 'rock') {
+    scoreC++;
     return `You chose ${human}, Computer chose ${computer}! You lose!`;
-  } 
+  }
 }
 
 //Event listener to allow rock, paper, scissors to be played in the browser
 startGame.addEventListener('click', () => {
   let userGuess = prompt("Enter rock, paper or scissors");
   userGuess = userGuess.toLowerCase();
-  if (!compareGuesses(userGuess, compGuess())) {
+  let cGuess = compGuess()
+  
+  if (!checkHumanIsValid(userGuess)) {
+    alert("Invalid Option, try again");
+
     output.innerText = "";
-  }
+  } 
   else {
-    output.innerText = compareGuesses(userGuess, compGuess());
-  }  
+    output.innerText = compareGuesses(userGuess, cGuess);
+  }
+
+  humanScore.innerText = scoreH;
+  compScore.innerText = scoreC;
 })
